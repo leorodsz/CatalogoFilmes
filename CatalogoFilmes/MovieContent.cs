380cf9c08f98b5cc64c0698bad1537c5d2430277
Léo Rodrigues <rleo3421@gmail.com>
2025-03-23 22:47:18 -0300

    enviando arquivos de catalogo de filmes

---
+++ CatalogoFilmes/MovieContent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoFilmes
+{
+    internal class MovieContent
+    {
+        public static string Conteudo(string conteudo){
+            switch (conteudo)
+            {
+                case "Minecraft":
+                    return "Baseado no famoso jogo, o filme deve trazer uma aventura no universo pixelado de Minecraft, com Steve Carrell no elenco.";
+                case "FNAF":
+                    return "Um segurança noturno começa a trabalhar na pizzaria Freddy Fazbear’s e descobre que os animatrônicos ganham vida à noite, tornando-se aterrorizantes.";
+                case "It the thing":
+                    return "Baseado no livro de Stephen King, segue um grupo de crianças que enfrenta Pennywise, um palhaço demoníaco que se alimenta de seus medos.";
+                default:
+                    return "Filme não encontrado";
+            }
+        }
+
+        public static string Horario(string horario)
+        {
+            switch (horario)
+            {
+                case "Minecraft":
+                    return "Horário de sessão: 14:00h - 15:20";
+                case "FNAF":
+                    return "Horário de sessão: 16:00h - 18:15";
+                case "It the thing":
+                    return "Horário de sessão: 18:00h - 20:00h";
+                default:
+                    return "Horário de sessão indisponivel";
+            }
+        }
+
+        public static string ImprimirIngresso(string filme)
+        {
+            string mensagem = " "; // Variável para armazenar a mensagem final
+
+            if (filme == "Minecraft")
+            {
+                string caminhoArquivo = "voucher.txt";
+                mensagem = "Ingresso impresso com sucesso para o filme do Minecraft. " + Horario("Minecraft") + Environment.NewLine;
+                File.AppendAllText(caminhoArquivo, mensagem); // Salva no arquivo
+            }
+            else if (filme == "FNAF")
+            {
+                string caminhoArquivo = "voucher.txt";
+                mensagem = "Ingresso impresso com sucesso para o filme do FNAF. " + Horario("FNAF") + Environment.NewLine;
+                File.AppendAllText(caminhoArquivo, mensagem);
+            }
+            else if (filme == "It the thing")
+            {
+                string caminhoArquivo = "voucher.txt";
+                mensagem = "Ingresso impresso com sucesso para o filme do It the thing. " + Horario("It the thing") + Environment.NewLine;
+                File.AppendAllText(caminhoArquivo, mensagem);
+            }
+            else
+            {
+                mensagem = "Filme não encontrado. Não foi possível imprimir o ingresso." + Environment.NewLine;
+                File.AppendAllText("voucher.txt", mensagem);
+            }
+
+            return mensagem;
+        }
+        }
+    }
---
+++ CatalogoFilmes/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CatalogoFilmes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0;
+            int op2 = 0;
+
+            do
+            {
+                Console.WriteLine("=======================");
+                Console.WriteLine("Catalogo de filmes");
+                Console.WriteLine("=======================");
+                Console.WriteLine("\n");
+                Console.WriteLine("Escolha o filme: ");
+                Console.WriteLine("[1] Minecraft");
+                Console.WriteLine("[2] FNAF");
+                Console.WriteLine("[3] It the thing");
+                Console.WriteLine("[4] Sair");
+                
+                // Captura a entrada do usuário e converte para inteiro
+                bool entradaValida = int.TryParse(Console.ReadLine(), out op);
+
+                if (!entradaValida)
+                {
+                    Console.WriteLine("\nOpção inválida! Digite um número entre 1 e 4.\n");
+                    continue;
+                    
+                }
+               
+                Thread.Sleep(1000);
+                Console.Clear();
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("\nVocê escolheu o filme do Minecraft");
+                        Console.WriteLine(MovieContent.Conteudo("Minecraft"));
+                        Console.WriteLine(MovieContent.Horario("Minecraft"));
+                        Console.WriteLine("\nDeseja imprimir seu ingresso?");
+                        Console.WriteLine("[1] Sim  ||  [2] Não");
+                        op2 = int.Parse(Console.ReadLine());
+                        if (op2 == 1)
+                        {
+                            
+                            string ingresso = MovieContent.ImprimirIngresso("Minecraft");
+                            Console.WriteLine(ingresso);
+                            // Abre o arquivo no bloco de notas
+                            Process.Start("notepad.exe", "voucher.txt");
+                        }
+                        else if (op2 == 2)
+                        {
+                            Console.WriteLine("Ingresso não impresso");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opção inválida! Digite apenas 1 ou 2.");
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nVocê escolheu o filme do FNAF");
+                        Console.WriteLine(MovieContent.Conteudo("FNAF"));
+                        Console.WriteLine(MovieContent.Horario("FNAF"));
+                        Console.WriteLine("\nDeseja imprimir seu ingresso?");
+                        Console.WriteLine("[1] Sim  ||  [2] Não");
+                        op2 = int.Parse(Console.ReadLine());
+                        if (op2 == 1)
+                        {
+                            // Chama o método para imprimir o ingresso e salvar no arquivo
+                            string ingresso = MovieContent.ImprimirIngresso("FNAF");
+                            Console.WriteLine(ingresso);
+                            // Abre o arquivo no bloco de notas
+                            Process.Start("notepad.exe", "voucher.txt");
+                        }
+                        else if (op2 == 2)
+                        {
+                            Console.WriteLine("Ingresso não impresso");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opção inválida! Digite apenas 1 ou 2.");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nVocê escolheu o filme do It the thing");
+                        Console.WriteLine(MovieContent.Conteudo("It the thing"));
+                        Console.WriteLine(MovieContent.Horario("It the thing"));
+                        Console.WriteLine("\nDeseja imprimir seu ingresso?");
+                        Console.WriteLine("[1] Sim  ||  [2] Não");
+                        op2 = int.Parse(Console.ReadLine());
+                        if (op2 == 1)
+                        {
+                            // Chama o método para imprimir o ingresso e salvar no arquivo
+                            string ingresso = MovieContent.ImprimirIngresso("It the thing");
+                            Console.WriteLine(ingresso);
+                            // Abre o arquivo no bloco de notas
+                            Process.Start("notepad.exe", "voucher.txt");
+                        }
+                        else if (op2 == 2)
+                        {
+                            Console.WriteLine("Ingresso não impresso");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opção inválida! Digite apenas 1 ou 2.");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nSaindo do programa...\n");
+                        Console.WriteLine("Obrigado por usar nosso catálogo de filmes!");
+                        break;
+
+                    default:
+                        Console.WriteLine("\nOpção inválida! Digite um número entre 1 e 4.\n");
+                        break;
+                }
+
+                Thread.Sleep(1000);
+                Console.Clear();
+
+            } while (op != 4);
+        }
+    }
+}
